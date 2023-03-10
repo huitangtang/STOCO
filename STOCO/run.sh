83b12b1d7a1a12e08bc1224a6482b3c834cd1dbe
HuiTang <42053362+huitangtang@users.noreply.github.com>
2023-03-10 15:57:50 +0800

    Update run.sh

--- STOCO/run.sh
+++ STOCO/run.sh
@@ -1,5 +1,5 @@
-python train.py  --dataset stl10  --root /workspace/data/  --num-labeled 1000  --arch wideresnet  --batch-size 64  --lr 0.03  --wdecay 0.0001  --expand-labels  --seed 1  --num_classifiers 5
-python train.py  --dataset stl10  --root /workspace/data/  --num-labeled 1000  --arch wideresnet  --batch-size 64  --lr 0.03  --wdecay 0.0001  --expand-labels  --seed 2  --num_classifiers 5
-python train.py  --dataset stl10  --root /workspace/data/  --num-labeled 1000  --arch wideresnet  --batch-size 64  --lr 0.03  --wdecay 0.0001  --expand-labels  --seed 3  --num_classifiers 5
-python train.py  --dataset stl10  --root /workspace/data/  --num-labeled 1000  --arch wideresnet  --batch-size 64  --lr 0.03  --wdecay 0.0001  --expand-labels  --seed 4  --num_classifiers 5
-python train.py  --dataset stl10  --root /workspace/data/  --num-labeled 1000  --arch wideresnet  --batch-size 64  --lr 0.03  --wdecay 0.0001  --expand-labels  --seed 5  --num_classifiers 5
+python train.py  --dataset cifar10  --root /data/cifar10/  --num-labeled 40  --arch wideresnet  --batch-size 64  --lr 0.03  --wdecay 0.0005  --expand-labels  --seed 1  --num_classifiers 5
+python train.py  --dataset cifar10  --root /data/cifar10/  --num-labeled 40  --arch wideresnet  --batch-size 64  --lr 0.03  --wdecay 0.0005  --expand-labels  --seed 2  --num_classifiers 5
+python train.py  --dataset cifar10  --root /data/cifar10/  --num-labeled 40  --arch wideresnet  --batch-size 64  --lr 0.03  --wdecay 0.0005  --expand-labels  --seed 3  --num_classifiers 5
+python train.py  --dataset cifar10  --root /data/cifar10/  --num-labeled 40  --arch wideresnet  --batch-size 64  --lr 0.03  --wdecay 0.0005  --expand-labels  --seed 4  --num_classifiers 5
+python train.py  --dataset cifar10  --root /data/cifar10/  --num-labeled 40  --arch wideresnet  --batch-size 64  --lr 0.03  --wdecay 0.0005  --expand-labels  --seed 5  --num_classifiers 5
